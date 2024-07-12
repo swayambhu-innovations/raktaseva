37f8506d963958606fc61465930aa1841e3b6375
KritarthShukla <143200473+KritarthShukla@users.noreply.github.com>
2024-07-12 17:37:45 +0530

    kritarth-task

--- projects/raktseva/src/app/profile/profile.component.ts
+++ projects/raktseva/src/app/profile/profile.component.ts
@@ -2,6 +2,14 @@ import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list'; 
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
+import { MatDialogRef } from '@angular/material/dialog';
+import { MatDialog } from '@angular/material/dialog';
+import { InviteFriendModalComponent } from '../invite-friend-modal/invite-friend-modal.component';
+import { MatBottomSheet } from '@angular/material/bottom-sheet';
+
+
 
 
 @Component({
@@ -14,6 +22,30 @@ import { MatListModule } from '@angular/material/list';
   styleUrl: './profile.component.scss'
 })
 export class ProfileComponent {
+  constructor(private route:Router ,private location: Location,private dialog: MatDialog){}
+
+  openInviteFriendModal() {
+    this.dialog.open(InviteFriendModalComponent, {
+      width: '500px', 
+      // height:' 500px',
+      panelClass: 'custom-dialog-container',
+      disableClose: true
+    });
+  }
+  routing(){
+    this.route.navigate(['editprofile'])
+  }
+
+
+
+
+
+
+
+  // editProfile() {
+  //   this.router.navigate(['/edit-profile']);
+  // }
+
 
 }
 
