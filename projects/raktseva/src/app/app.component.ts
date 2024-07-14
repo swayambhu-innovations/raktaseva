e0c9959781ae5384eca78dd590ab6b1b4b720e57
sadhanachandel99 <168443062+sadhanachandel99@users.noreply.github.com>
2024-07-14 03:10:21 -0400

    donerservey radio

--- projects/raktseva/src/app/app.component.ts
+++ projects/raktseva/src/app/app.component.ts
@@ -4,13 +4,14 @@ import { RequirementFormComponent } from "./requirement-form/requirement-form.co
 import { EditProfileComponent } from './edit-profile/edit-profile.component';
 import { ProfileComponent } from './profile/profile.component';
 import { InviteFriendModalComponent } from './invite-friend-modal/invite-friend-modal.component';
+import { HomeComponent } from "./home/home.component";
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, EditProfileComponent, ProfileComponent, RequirementFormComponent,InviteFriendModalComponent],
+  imports: [RouterOutlet, EditProfileComponent, ProfileComponent, RequirementFormComponent, InviteFriendModalComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
