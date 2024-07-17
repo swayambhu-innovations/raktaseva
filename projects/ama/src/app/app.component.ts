2c5b743ac9451f146047fb416999a902e1c7b580
sadhanachandel99 <168443062+sadhanachandel99@users.noreply.github.com>
2024-07-17 05:25:39 -0400

    login page

--- projects/ama/src/app/app.component.ts
+++ projects/ama/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { CancelledComponent } from "./modules/cancelled/cancelled.component";
 import { PendingComponent } from "./modules/pending/pending.component";
 import { UserAndPermissionComponent } from "./user-and-permission/user-and-permission.component";
+import { LoginComponent } from "./auth/login/login.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CancelledComponent, UserAndPermissionComponent, PendingComponent],
+  imports: [RouterOutlet, CancelledComponent, UserAndPermissionComponent, PendingComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
