edad3d8d2ac16dc789c0d1efa9a4653ee61261dd
sadhanachandel99 <168443062+sadhanachandel99@users.noreply.github.com>
2024-07-17 03:22:15 -0400

    pending page completed

--- projects/ama/src/app/app.component.ts
+++ projects/ama/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CancelledComponent } from "./modules/cancelled/cancelled.component";
+import { PendingComponent } from "./modules/pending/pending.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CancelledComponent],
+  imports: [RouterOutlet, CancelledComponent, PendingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
