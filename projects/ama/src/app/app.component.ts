2a281e67835a2232986aa8ba798eec0bd4e8c939
ambarmishraa <ambarmishra740@gmail.com>
2024-07-17 15:18:30 +0530

    auth component created

--- projects/ama/src/app/app.component.ts
+++ projects/ama/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CancelledComponent } from "./modules/cancelled/cancelled.component";
+import { UserAndPermissionComponent } from "./user-and-permission/user-and-permission.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CancelledComponent],
+  imports: [RouterOutlet, CancelledComponent, UserAndPermissionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
