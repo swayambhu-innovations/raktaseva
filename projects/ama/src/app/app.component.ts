de62edcb8ab594abcd07bc919e2e1e03925f56b1
KritarthShukla <143200473+KritarthShukla@users.noreply.github.com>
2024-07-18 17:00:01 +0530

    Dashboard/kritarth

--- projects/ama/src/app/app.component.ts
+++ projects/ama/src/app/app.component.ts
@@ -4,12 +4,20 @@ import { CancelledComponent } from "./modules/cancelled/cancelled.component";
 import { PendingComponent } from "./modules/pending/pending.component";
 import { UserAndPermissionComponent } from "./user-and-permission/user-and-permission.component";
 import { LoginComponent } from "./auth/login/login.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { TotalRequestComponent } from "./dashboard/charts/total-request/total-request.component";
+import { ApprovedRequestComponent } from "./dashboard/charts/approved-request/approved-request.component";
+import { ChartContainerComponent } from "./dashboard/charts/chart-container/chart-container.component";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { OverallReportComponent } from "./dashboard/charts/overall-report/overall-report.component";
+import { StatisticsComponent } from "./dashboard/charts/statistics/statistics.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CancelledComponent, UserAndPermissionComponent, PendingComponent, LoginComponent],
+  imports: [RouterOutlet, CancelledComponent, UserAndPermissionComponent, PendingComponent, LoginComponent, DashboardComponent, TotalRequestComponent, ApprovedRequestComponent, ChartContainerComponent, OverallReportComponent, StatisticsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ projects/ama/src/app/dashboard/charts/total-request/total-request.component.ts
@@ -0,0 +1,101 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'; 
+
+
+@Component({
+  selector: 'app-total-request',
+  standalone: true,
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    CommonModule,
+    FormsModule,
+  
+  ],
+  
+  templateUrl: './total-request.component.html',
+  styleUrls: ['./total-request.component.scss']
+})
+export class TotalRequestComponent implements OnInit {
+  startDate!: Date;
+  endDate!: Date;
+  maxDate: string;
+  private myChart: any;
+  fromDate: any = '';
+  toDate: any = '';
+  keysArray: string[] = [];
+  valuesArray: string[] = [];
+  @Output() notify = new EventEmitter<any>();
+
+  constructor() {
+    this.maxDate = new Date().toISOString().split('T')[0];
+  }
+
+  ngOnInit() {
+    const currentDate = new Date();
+    this.startDate = new Date(currentDate);
+    this.endDate = new Date();
+    this.startDate.setDate(currentDate.getDate() - 6); 
+    this.onDateChange(); 
+  }
+
+  onDateChange() {
+    this.fromDate = this.convertToYYYYMMDD(this.startDate.toString());
+    this.toDate = this.convertToYYYYMMDD(this.endDate.toString());
+    console.log(this.fromDate, this.toDate);
+  }
+
+  convertToYYYYMMDD(dateString: string): string | null {
+    const inputDate = new Date(dateString);
+    if (isNaN(inputDate.getTime())) {
+      return null;
+    }
+    const year = inputDate.getFullYear();
+    const month = ('0' + (inputDate.getMonth() + 1)).slice(-2); 
+    const day = ('0' + inputDate.getDate()).slice(-2);
+    return `${year}-${month}-${day}`;
+  }
+
+  // private destroyChart() {
+  //   if (this.myChart) {
+  //     this.myChart.destroy();
+  //     this.myChart = null;
+  //   }
+  // }
+
+  // private loadChart() {
+  //   if (this.myChart) {
+  //     this.destroyChart();
+  //   }
+  //   this.myChart = new Chart('bookingsPerDay', {
+  //     type: 'line',
+  //     data: {
+  //       labels: this.keysArray,
+  //       datasets: [
+  //         {
+  //           label: 'Sales',
+  //           data: this.valuesArray,
+  //           borderColor: 'rgb(255, 99, 132)',
+  //           backgroundColor: 'rgb(255, 99, 132,50)',
+  //           fill: true,
+  //           tension: 0.4,
+  //         },
+  //       ],
+  //     },
+  //     options: {
+  //       plugins: {
+  //         legend: {
+  //           display: false,
+  //         },
+  //       },
+  //     },
+  //   });
+  // }
+}
