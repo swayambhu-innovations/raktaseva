f477c66ed5d2251df40517b2bb1762be50fe3d40
sadhanachandel99 <168443062+sadhanachandel99@users.noreply.github.com>
2024-07-19 05:25:37 -0400

    login and logout completed

--- projects/ama/src/app/modules/pending/pending.component.ts
+++ projects/ama/src/app/modules/pending/pending.component.ts
@@ -1,12 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { PatientDetailsComponent } from "./patient-details/patient-details.component";
 
 @Component({
   selector: 'app-pending',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, PatientDetailsComponent],
   templateUrl: './pending.component.html',
   styleUrl: './pending.component.scss'
 })
 export class PendingComponent {
+  isOpen = false;
 
+  openModal() {
+      this.isOpen = true;
+  }
+
+  closeModal() {
+      this.isOpen = false;
+  }
 }
+
+
+
+
