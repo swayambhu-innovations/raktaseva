19db87efe9e5f58d2415506533bb9ca35439385d
ambarmishraa <ambarmishra740@gmail.com>
2024-07-20 17:15:35 +0530

    user and permission designing

--- projects/ama/src/app/user-and-permission/email.service.ts
+++ projects/ama/src/app/user-and-permission/email.service.ts
@@ -1,23 +1,31 @@
 import { Injectable } from '@angular/core';
-import { from, Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { catchError, from, Observable, tap } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class EmailService {
-  private emailUrl = 'https://sendemail-xrrbnpwedq-uc.a.run.app';
+  private url = 'https://sendemail-xrrbnpwedq-uc.a.run.app';
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*', // Allow requests from all origins
+      Accept: '*/*',
+      'Access-Control-Allow-Methods': 'HEAD, GET, POST, PUT, PATCH, DELETE',
+       'Access-Control-Allow-Headers': 'Content-Type,X-API-KEY,X-Requested-With,Accept,  X-Auth-Token, Origin, Authorization,Access-Control-Request-Method,Access-Control-Request-Headers',
+       
+    }),
+  };
 
   constructor(private http: HttpClient) {}
 
   sendEmail(to: string, subject: string, text: string) {
     const emailData = { to, subject, text };
-    console.log(emailData);
-    return this.http
-      .post<Observable<any>>(this.emailUrl, emailData)
-      .subscribe((res) => {
-        console.log(res);
-      });
+    return this.http.post(this.url, emailData, this.httpOptions);
   }
+  // sendEmail(to: string, subject: string, text: string): Observable<any> {
+  //   const emailData = { to, subject, text };
+  //   return this.http.post(this.url, emailData,this.httpOptions);
+  // }
 }
-
