1f698add9e878d42a3f2bf8db457009b81d7c69f
ambarmishraa <ambarmishra740@gmail.com>
2024-07-25 17:00:18 +0530

    receiver detail page completed

--- projects/bloodbank/src/app/app.component.ts
+++ projects/bloodbank/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { DonorDetailComponent } from "./donor-detail/donor-detail.component";
 import { SidebarComponent } from "../../../ama/src/app/sidebar/sidebar.component";
 import { BbsidebarComponent } from "./shared/bbsidebar/bbsidebar.component";
+import { ReceiverDetailComponent } from "./receiver-detail/receiver-detail.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, BbsidebarComponent, DonorDetailComponent],
+  imports: [RouterOutlet, BbsidebarComponent, DonorDetailComponent, ReceiverDetailComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
