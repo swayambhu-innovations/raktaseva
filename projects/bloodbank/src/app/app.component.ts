96be83a73b80e81abe6a3c9d0745e975dd9de898
KritarthShukla <143200473+KritarthShukla@users.noreply.github.com>
2024-07-27 17:32:34 +0530

    app

--- projects/bloodbank/src/app/app.component.ts
+++ projects/bloodbank/src/app/app.component.ts
@@ -5,7 +5,6 @@ import { SidebarComponent } from "./sidebar/sidebar.component";
 import { LoginComponent } from "./auth/login/login.component";
 import { NotAdminComponent } from "./auth/not-admin/not-admin.component";
 import { DonorDetailComponent } from "./donor-detail/donor-detail.component";
-import { SidebarComponent } from "../../../ama/src/app/sidebar/sidebar.component";
 import { BbsidebarComponent } from "./shared/bbsidebar/bbsidebar.component";
 import { ReceiverDetailComponent } from "./receiver-detail/receiver-detail.component";
 import { AssignDonorDialogComponent } from "./receiver-detail/assign-donor-dialog/assign-donor-dialog.component";
